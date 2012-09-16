230826a4fce94989f2f9bc42a7c9edf67be6ad95
bichotll <jaume@bic.cat>
2012-09-16 21:00:09 +0200

    mes canvis

---
+++ bic_calendar/example/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$mes = $_GET['mes'];
+$ano = $_GET['ano'];
+
+$array = array(
+	array("27/$mes/$ano", 'Getting Contacts Barcelona', 'http://gettingcontacts.com/events/view/barcelona'),
+	array("7/$mes/$ano", '2º Getting Contacts Alaquás', 'http://gettingcontacts.com/events/view/alaquas', '#3B4', 'contingut popover<img src="http://gettingcontacts.com/upload/news/estiu_productiu.png" >')
+);
+
+
+
+echo json_encode($array);
+
+?>
